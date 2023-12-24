297f7d51dd46d5492aa2235c08814bd2219876a3
Eluamous-droid <themik1337@gmail.com>
2023-12-24 14:27:10 +0100

    day 1 task 2

--- day1.go
+++ day1.go
@@ -9,7 +9,9 @@ func GetSumOfCalibrationNumbers(fileLines []string) int{
 	
 	sum := 0;
 	for _,line := range fileLines{
-		digits := findFirstDigit(line) + findLastDigit(line)
+		actualLine := replaceWordWithDigit(line)
+		digits := findFirstDigit(actualLine) + findLastDigit(actualLine)
+		println(digits)
 		var number int
 		number,_ = strconv.Atoi(digits)
 
@@ -17,6 +19,20 @@ func GetSumOfCalibrationNumbers(fileLines []string) int{
 	}
 	return sum
 
+}
+
+func replaceWordWithDigit(line string) string{
+	newLine := strings.ReplaceAll(line, "one", "o1e")
+	newLine = strings.ReplaceAll(newLine, "two", "t2o")
+	newLine = strings.ReplaceAll(newLine, "three", "t3ree")
+	newLine = strings.ReplaceAll(newLine, "four", "f4or")
+	newLine = strings.ReplaceAll(newLine, "five", "f5ve")
+	newLine = strings.ReplaceAll(newLine, "six", "s6x")
+	newLine = strings.ReplaceAll(newLine, "seven", "s7ven")
+	newLine = strings.ReplaceAll(newLine, "eight", "e8ght")
+	newLine = strings.ReplaceAll(newLine, "nine", "n9ne")
+
+	return newLine
 }
 
 func findFirstDigit(line string) string{
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module adventOfCode
 
-go 1.21.5
+go 1.20
